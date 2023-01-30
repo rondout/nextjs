a490fc3d5c2251b43d895d7cddb81181b931dd69
rondout <59380464+rondout@users.noreply.github.com>
2023-01-30 15:05:34 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // 默认语言还可以通过识别域名来设置
   // https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
-    locales: ["en", "zh"],
+    locales: ["zh", "en"],
     defaultLocale: "en",
     localeDetection: false,
     domains: [
