fcf4ac425e54d35ef58820376932058dd9936864
rondout <59380464+rondout@users.noreply.github.com>
2023-01-30 13:39:46 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,10 +3,10 @@ module.exports = {
   // https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en", "zh"],
-    defaultLocale: "zh",
+    defaultLocale: "en",
     localeDetection: false,
     domains: [
-      { domain: "nextjs-rondout.vercel.app", defaultLocale: "zh" },
+      { domain: "vercel.app", defaultLocale: "zh" },
       { domain: "ctort.com:3001", defaultLocale: "en", http: true },
     ],
   },
